908e6cebe590915fa3ca7263d6ab1b4819a3ba26
anthonychen00 <tomokoNALS@gmail.com>
2018-12-24 12:49:14 -0800

    Prints novels that have been updated

--- NU_Scraper.py
+++ NU_Scraper.py
@@ -1,5 +1,6 @@
 import requests, lxml.html
 import getpass
+import sys
 from bs4 import BeautifulSoup
 
 def login():
@@ -20,25 +21,31 @@ def scrapper():
         form['pwd'] = password
         response = s.post('https://www.novelupdates.com/login/',data=form)
         if response.url == 'https://www.novelupdates.com/':
-            print("login sucessful")
+            print("Login Sucessful")
             reading_list = s.get('https://www.novelupdates.com/reading-list/')
             # reading_html = lxml.html.fromstring(reading_list.text)
             # table_entries = reading_html.xpath('//tbody/tr/td/a')
             return reading_list.text
         else:
             print("Error Unable to log on")
+            obj = open("Error.html","w")
+            obj.write(response.text)
     else:
         print("Failure to access login page")
-        return None
+        sys.exit()
 
 def html_parse(html):
     titles = []
     current_chapters = []
     latest_chapters = []
     soup = BeautifulSoup(html,'html.parser')
+    print("Parsing Reading List")
     title_names = soup.find_all("tr",attrs={"class":"rl_links"})
     chapters = soup.find_all("a",attrs={"class":"chp-release"})
     counter = 0;
+    if (len(chapters)%2) != 0:
+        print("Error: Incorrect number of chapters")
+        sys.exit()
     for i in range(len(title_names)):
         titles.append(title_names[i].attrs["data-title"])
         current_chapters.append(chapters[i + counter].get_text())
@@ -46,7 +53,22 @@ def html_parse(html):
         counter += 1
     return(titles, current_chapters, latest_chapters)
 
-#temp = soup.find_all('a',attrs{"class":"chp_release"})
+
+def newChapters(titles, current_chapters, latest_chapters):
+    num_of_books = len(titles)
+    posted = []
+    for i in range(num_of_books):
+        if current_chapters[i] != latest_chapters[i]:
+            posted.append(titles[i])
+    if len(posted) != 0:
+        if len(posted) > 1:
+            print("New chapters have been posted for...")
+        else:
+            print("A new chapter has been posted for...")
+        for post in posted:
+            print("-    " + post)
+
 
 html_string = scrapper()
 titles, current_chapters, latest_chapters = html_parse(html_string)
+newChapters(titles, current_chapters, latest_chapters)
