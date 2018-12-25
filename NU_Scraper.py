598482793237dc451f23f5f50c462ffb79c5b24c
anthonychen00 <tomokoNALS@gmail.com>
2018-12-24 19:07:34 -0800

    Began a text-based user interface

--- NU_Scraper.py
+++ NU_Scraper.py
@@ -2,13 +2,14 @@ import requests, lxml.html
 import getpass
 import sys
 from bs4 import BeautifulSoup
+from novel import Novel
 
 def login():
     username = input("Enter your NovelUpdates Username: ")
     password = getpass.getpass("Enter your NovelUpdates Password: ")
     return username, password
 
-def scrapper():
+def scrapper(username, password):
     # logs onto website and pulls in the data from reading list onto a file
     s = requests.session()
     response = s.get('https://www.novelupdates.com/login/', timeout = 5)
@@ -16,30 +17,26 @@ def scrapper():
         login_html = lxml.html.fromstring(response.text)
         hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
         form = {x.attrib["name"]:x.attrib["value"] for x in hidden_inputs}
-        username, password = login();
         form['log'] = username
         form['pwd'] = password
         response = s.post('https://www.novelupdates.com/login/',data=form)
+
         if response.url == 'https://www.novelupdates.com/':
-            print("Login Sucessful")
+            print("\t  ===Login Sucess===")
             reading_list = s.get('https://www.novelupdates.com/reading-list/')
-            # reading_html = lxml.html.fromstring(reading_list.text)
-            # table_entries = reading_html.xpath('//tbody/tr/td/a')
             return reading_list.text
         else:
-            print("Error Unable to log on")
-            obj = open("Error.html","w")
-            obj.write(response.text)
+            print("Error: Unable to log on, please check credentials")
+            sys.exit()
     else:
         print("Failure to access login page")
         sys.exit()
 
 def html_parse(html):
-    titles = []
-    current_chapters = []
-    latest_chapters = []
+    reading_list = []
     soup = BeautifulSoup(html,'html.parser')
-    print("Parsing Reading List")
+    print("\t Parsing Reading List")
+    print(" ====================================")
     title_names = soup.find_all("tr",attrs={"class":"rl_links"})
     chapters = soup.find_all("a",attrs={"class":"chp-release"})
     counter = 0;
@@ -47,28 +44,53 @@ def html_parse(html):
         print("Error: Incorrect number of chapters")
         sys.exit()
     for i in range(len(title_names)):
-        titles.append(title_names[i].attrs["data-title"])
-        current_chapters.append(chapters[i + counter].get_text())
-        latest_chapters.append(chapters[i+ counter + 1].get_text())
+        title = title_names[i].attrs["data-title"]
+        current_chapter = chapters[i + counter].get_text()
+        latest_chapter = chapters[i+ counter + 1].get_text()
         counter += 1
-    return(titles, current_chapters, latest_chapters)
+        temp = Novel(title,latest_chapter, current_chapter)
+        reading_list.append(temp)
+    return(reading_list)
 
+def newChapters(reading_list, userPrint):
+    numOfCh = 0
+    for novel in reading_list:
+        if novel.new_update():
+            if userPrint:
+                print(" -  " + novel.title)
+            else:
+                numOfCh += 1
+    if userPrint:
+        return 0
+    return(numOfCh)
 
-def newChapters(titles, current_chapters, latest_chapters):
-    num_of_books = len(titles)
-    posted = []
-    for i in range(num_of_books):
-        if current_chapters[i] != latest_chapters[i]:
-            posted.append(titles[i])
-    if len(posted) != 0:
-        if len(posted) > 1:
-            print("New chapters have been posted for...")
-        else:
-            print("A new chapter has been posted for...")
-        for post in posted:
-            print("-    " + post)
+def promptTitles(updates):
+    userInput = "n"
+    if updates > 1:
+        print("There are " + str(updates) + " chapters released!")
+        userInput = input("Show which novel titles? (y/n)")
+    elif updates == 1:
+        print("There is a new chapter released!")
+        userInput = input("Show the novel title? (y/n)")
+    while (userInput != 'n' and userInput != 'y'):
+        print("Error: Invalid Input")
+        if updates > 1:
+            userInput = input("Show which novel titles? (y/n)")
+        elif updates == 1:
+            userInput = input("Show the novel title? (y/n)")
+    if userInput == "y":
+        if updates > 1:
+            print("The updates are for: ...")
+        elif updates == 1:
+            print("The update is for: ... ")
+        return True
+    else:
+        return False
 
-
-html_string = scrapper()
-titles, current_chapters, latest_chapters = html_parse(html_string)
-newChapters(titles, current_chapters, latest_chapters)
+if __name__ == '__main__':
+    username, password = login()
+    html_string = scrapper(username,password)
+    readingList = html_parse(html_string)
+    updates = newChapters(readingList, userPrint=False)
+    userPrint = promptTitles(updates)
+    updates = newChapters(readingList, userPrint)
